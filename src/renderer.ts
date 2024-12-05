f171cfc031a0a5fae66593678ac01e152de92013
Vrishin Patel <vrishin_patel@outlook.com>
2024-12-05 23:04:19 +0530

    renderer methods to add and remove pets to and from the world

--- src/renderer.ts
+++ src/renderer.ts
@@ -3,12 +3,15 @@ import {
 	type DefaultLoader,
 	DisplayMode,
 	Engine,
+	Scene,
 	Vector,
 } from 'excalibur';
 import { Pet } from './pet';
 
 // Roadmap: Refactor to use only the WebGL/Canvas renderer instead of the entire game engine.
 export class Renderer extends Engine {
+	world = new Scene();
+
 	constructor() {
 		super({
 			displayMode: DisplayMode.FillScreen,
@@ -16,19 +19,29 @@ export class Renderer extends Engine {
 		});
 	}
 
+	// Should probably split into joinWorld and addPetToWorld to leverage the unkill() method on Actors.
+	public addPetToWorld(imageUrl: string, username: string) {
+		const pet = new Pet(imageUrl, {
+			height: 100,
+			width: 100,
+			pos: new Vector(0, 0),
+		});
+
+		// TODO: Add a label on the pet that contains the username
+		this.world.add(pet);
+		return pet;
+	}
+
+	public removePetFromWorld(pet: Pet) {
+		pet.kill();
+	}
+
 	onInitialize() {
-		this.rootScene.camera.pos = new Vector(0, 0);
+		this.world.camera.pos = new Vector(0, 0);
 	}
 
 	start(loader: DefaultLoader) {
-		this.add(
-			new Pet('/black-rex.png', {
-				height: 100,
-				width: 100,
-				pos: new Vector(0, 0),
-			}),
-		);
-
+		this.add('world', this.world);
 		return super.start(loader);
 	}
 }
